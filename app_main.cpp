b0e681d7b7e28c835bf94246022dc5d146ce619f
Hoda Magdy <hodamagdyhussein@aucegypt.edu>
2026-05-10 21:14:46 +0300

    face detection working - FireBeetle 2 ESP32-S3

---
+++ app_main.cpp
@@ -0,0 +1,11 @@
+#include "who_camera.h"
+#include "who_human_face_detection.hpp"
+
+static QueueHandle_t xQueueAIFrame = NULL;
+
+extern "C" void app_main()
+{
+    xQueueAIFrame = xQueueCreate(2, sizeof(camera_fb_t *));
+    register_camera(PIXFORMAT_RGB565, FRAMESIZE_240X240, 1, xQueueAIFrame);
+    register_human_face_detection(xQueueAIFrame, NULL, NULL, NULL, true);
+}
